Dataset division fixed and overflow/underflow fixed

perceptron_softmax.py
@@ -1,4 +1,7 @@
 #Rede com 3 perceptrons (Exemplo IRIS) 
+
+#Site iris: https://archive.ics.uci.edu/ml/datasets/iris
+
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,13 +15,13 @@ def sum(amostra, weights, bias):
         sum_perceptron = np.dot(amostra, weights[i]) + bias[i]
         sum_perceptrons[i] = sum_perceptron
 
-    # print('Sum perceptrons: ', sum_perceptrons)
     return sum_perceptrons
 
 # A função de ativação utilizada foi a softmax
 def softmax(valor):
-    probs = np.exp(valor) / np.sum(np.exp(valor))
-    # print('Softmax: ', probs)
+    # Note que subtraímos de todos os valores do vetor, o valor do maior elemento. Dessa forma, garantimos que não ocorrerá um overflow ou underflow no cálculo de exponencial.
+    novo_valor = valor - max(valor)
+    probs = np.exp(novo_valor) / np.sum(np.exp(novo_valor))
     return probs
 
 # Função de previsão
@@ -49,20 +52,21 @@ def treinamento(amostras, classes, taxa_aprendizado, max_iteracoes):
             print("Previsão ({}) => Classe ({})".format(saida.argmax(axis=0),classe))
             
             error = np.zeros(len(saida))
+            
+            previsto = np.argmax(saida)
 
             for i in range(len(saida)):
-                if(i == classe):
+                if(i == previsto and previsto == classe):
                     error[i] =  1.0 - saida[i]
-                    if math.isnan(error[i]): error[i] = 0
-
+                        
                 else:
                     error[i] = 0.0 - saida[i]
-                    if math.isnan(error[i]): error[i] = 0
                     
                 bias[i] += taxa_aprendizado * error[i]
 
                 for j in range(weights.shape[1]):
                     weights[i][j] += taxa_aprendizado * error[i] * amostra[j]
+                    if math.isnan(weights[i][j]): sys.exit()
 
             quad_error += np.sum(error**2)
             print('quad_error', quad_error)
@@ -72,19 +76,20 @@ def treinamento(amostras, classes, taxa_aprendizado, max_iteracoes):
         if(quad_error == 0):
             break    
         
-
     return bias, weights, quad_errors
     
 # Validação do método (Verificando acurácia)
 
 def validacao(amostras, classes, bias, weights):
     errors = 0
+
+    print('Weights em validação: ', weights)
+    print('Bias em validação: ', bias)
+
     for amostra, classe in zip(amostras, classes):
-            # print("Classificando amostra = {} (classe {})".format(amostra, classe))
             saida = previsao(amostra, weights, bias)
-            # print("Previsão ({}) => Classe ({})".format(saida.argmax(axis=0),classe))
-            
             if(saida.argmax(axis=0) != classe): errors += 1
+
     print('{} erros na classificação!'.format(errors))
     acuracia = (amostras.shape[0] - errors) / amostras.shape[0]
 
@@ -100,18 +105,32 @@ print(classes)
 print('Número de amostras: ', len(amostras))
 print('Número de classes: ', len(classes))
 
-amostras_treinamento = amostras[:int(70/100*amostras.shape[0])]
-amostras_validacao = amostras[int(70/100*amostras.shape[0]):]
 
-classes_treinamento = classes[:int(70/100*classes.shape[0])]
-classes_validacao = classes[int(70/100*classes.shape[0]):]
+amostras_setosa = amostras[:50]
+amostras_versicolour = amostras[50:100]
+amostras_virginica = amostras[100:]
+
+qtde_treino =  int((70/100)*amostras_setosa.shape[0])
+
+classes_setosa = classes[:50]
+classes_versicolour = classes[50:100]
+classes_virginica = classes[100:]
+
+amostras_treinamento = np.concatenate((amostras_setosa[:qtde_treino], amostras_versicolour[:qtde_treino], amostras_virginica[:qtde_treino]), axis = 0)
+amostras_validacao = np.concatenate((amostras_setosa[qtde_treino:], amostras_versicolour[qtde_treino:], amostras_virginica[qtde_treino:]), axis = 0)
+
+print(amostras_validacao)
+
+classes_treinamento = np.concatenate((classes_setosa[:qtde_treino], classes_versicolour[:qtde_treino], classes_virginica[:qtde_treino]), axis = 0)
+classes_validacao = np.concatenate((classes_setosa[qtde_treino:], classes_versicolour[qtde_treino:], classes_virginica[qtde_treino:]), axis = 0)
+
 
 print('Número de amostras treinamento: ', len(amostras_treinamento))
 print('Número de classes treinamento: ', len(classes_treinamento))
 print('Número de amostras validação: ', len(amostras_validacao))
 print('Número de classes validação: ', len(classes_validacao))
 
-bias, weights, quad_errors = treinamento(amostras_treinamento, classes_treinamento, taxa_aprendizado=0.01, max_iteracoes=50)
+bias, weights, quad_errors = treinamento(amostras_treinamento, classes_treinamento, taxa_aprendizado=0.01, max_iteracoes=800)
 
 print('Matriz de pesos final: ', weights)
 print('Bias final: ', bias)